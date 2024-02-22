58e7cfdca56264aae17a2edc2f3a91dd5f1a0b0f
OUALIID <walidelhadim8@gmail.com>
2024-02-22 01:07:58 +0100

    Configure mock objects to return specific values in test cases

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,8 @@ Test cases for the utils.access_nested_map function.
 """
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -28,6 +29,24 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test access_nested_map function with invalid inputs."""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Test cases for the get_json function."""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """Test get_json function."""
+        with patch("requests.get") as mock_get:
+            mock_response = Mock()
+            mock_response.json.return_value = test_payload
+            mock_get.return_value = mock_response
+
+            result = get_json(test_url)
+            self.assertEqual(result, test_payload)
 
 
 if __name__ == '__main__':
