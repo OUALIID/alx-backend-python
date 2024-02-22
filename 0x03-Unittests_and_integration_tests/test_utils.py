215cf4b9cb7a42f900ece7971e1ab76bed37f152
OUALIID <walidelhadim8@gmail.com>
2024-02-22 02:08:35 +0100

    Test the memoization functionality of the memoize decorator

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@ Test cases for the utils.access_nested_map function.
 """
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from unittest.mock import patch, Mock
 
 
@@ -47,6 +47,28 @@ class TestGetJson(unittest.TestCase):
 
             result = get_json(test_url)
             self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """Test cases for the memoize decorator."""
+
+    def test_memoize(self):
+        """Test the memoization functionality of the memoize decorator"""
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method',
+                          return_value=42) as mock_a_method:
+            test_instance = TestClass()
+            self.assertEqual(test_instance.a_property, 42)
+            self.assertEqual(test_instance.a_property, 42)
+            mock_a_method.assert_called_once()
 
 
 if __name__ == '__main__':
