813df4dea4701089539c6e46436d86683ca9916c
OUALIID <walidelhadim8@gmail.com>
2024-02-22 04:54:30 +0100

    Test the org method of GithubOrgClient.

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -17,5 +17,5 @@ class TestGithubOrgClient(unittest.TestCase):
         """Test the org method of GithubOrgClient."""
         mock_get_json.return_value = {"name": org_name}
         self.assertEqual(GithubOrgClient(org_name).org(), {"name": org_name})
-        mock_get_json.assert_called_once_with(
-            f'https://api.github.com/orgs/{org_name}')
+        url = f'https://api.github.com/orgs/{org_name}'
+        mock_get_json.assert_called_once_with(url)
